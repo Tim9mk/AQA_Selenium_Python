31741da7323aafce1b3243158c5dea45833183a7
Timur <98800581+Tim9mk@users.noreply.github.com>
2025-05-13 22:43:40 +0300

    add logic for separate path to chromedriver

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -9,13 +10,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver(request):
     # Настройка опций для Chrome
     options = Options()
-    options.binary_location = "/usr/bin/chromium-browser"
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    service = Service("/usr/bin/chromedriver")
+    is_docker = os.path.exists('/.dockerenv')
+
+    if is_docker:
+        service = Service("/usr/bin/chromedriver")
+    else:
+        service = Service(ChromeDriverManager().install())
+
     driver = webdriver.Chrome(service=service, options=options)
     request.cls.driver = driver
 
